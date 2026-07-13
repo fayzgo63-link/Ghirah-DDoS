5554aa64b8a01a262c2f5410f74a27f3b4220574
fayzgo63-link <fayzgo63@gmail.com>
2026-07-13 11:34:11 +0700

    Implement user login functionality
    Add user authentication with username and password prompts.

--- goo.py
+++ goo.py
@@ -9,7 +9,20 @@ import requests
 import threading
 from queue import Queue
 
+
+attemps = 0
 os.system("clear")
+while attemps < 100:
+    username = input("\033[32mEnter your username: \033[0m")
+    password = input("\033[31mEnter your password: \033[0m")
+
+    if username == 'bp4' and password == 'bp4':
+        print("\033[32m⟩⟩ Hai...! Welcome to zona attack BLACKPHANTER \033[0m")
+        break
+    else:
+        print('Incorrect credentials. Check if you have Caps lock on and try again.')
+        attemps += 1
+        continue
 
 # -----------------------------------
 

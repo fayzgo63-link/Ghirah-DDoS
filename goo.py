4142958801e547dd068aae6d4c6a956cb56df4bf
fayzgo63-link <fayzgo63@gmail.com>
2026-07-13 19:48:54 +0700

    Update goo.py

--- goo.py
+++ goo.py
@@ -28,10 +28,10 @@ print("""
                              \033[37m╰───╮│││   ││  ││││     ╰───╮│
                              \033[37m╭───╯││╰──╮│╰──╯│││     ╭───╯│
                              \033[37m╰───╯ ╰───╯╰────╯╰╯     ╰────╯
-\033[33m╭╮───╮  ╭──╮  ╭╮   ┌───  ╭───╮┌────┐ ╭╮
-\033[33m││   │ │    │ ││   ││    ││     ││   ││
-\033[33m│╭───╯ │╭──╮│ ││   └──   ╰───╮  ││  ││
-\033[33m╰╯     ╰╯  ╰╯ ╰───╯ └───  ───╯  ╰╯  ╰╯
+\033[33;1m╭╮───╮  ╭──╮  ╭╮   ┌───  ╭───╮┌────┐ ╭╮
+\033[33;1m││   │ │    │ ││   ││    ││     ││   ││
+\033[33;1m│╭───╯ │╭──╮│ ││   └──   ╰───╮  ││  ││
+\033[33;1m╰╯     ╰╯  ╰╯ ╰───╯ └───  ───╯  ╰╯  ╰╯
 """)
 while attemps < 100:                             
     username = input("\033[33mEnter your username: \033[30m")
@@ -170,7 +170,7 @@ def check_proxy(proxy): # Detecting proxy TCP connections and HTTP requests
         with lock:
             conns += 1
             good_proxies.append(proxy)
-            print(f"\033[38;5;206mL0veStory\033[37m=>proxy: \033[35m{proxy_ip:^15s}\033[0m port: \033[33;1m{str(proxy_port):^5s}\033[0m conns: \033[34m{str(conns):^4s}\033[0m >{proto:^5s} \033[32mConnected\033[0m")
+            print(f"\033[38;5;206mL0veStory\033[37m=>proxy: \033[35m{proxy_ip:^15s}\033[0m port: \033[32m{str(proxy_port):^5s}\033[0m conns: \033[34m{str(conns):^4s}\033[0m >{proto:^5s} \033[33mConnected\033[0m")
             print(f'\33]0;[{conns}] Proxies | ProxyChecker Code By GogoZin\a',end='')
     except Exception as e:
         pass
